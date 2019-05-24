725f0a6a436b9880229325e22ebac88a315734d1
Irene Truong <irenesis123@gmail.com>
2019-05-24 16:37:34 -0400

    added commentary to scripts

--- scripts.js
+++ scripts.js
@@ -40,10 +40,6 @@ $.when(...pokeParty)
     .then((...pokeChoices) => {
         // go through pokeChoices and grab the first item[0] of each of the objects
         const arrayOfChoices = pokeChoices.map(pokemon => pokemon[0]);
-        // let globalArray = arrayOfChoices;
-        // console.log(arrayOfChoices);
-        // console.log(globalArray);
-        // Associated random number with the object in arrayOfChoices
         let card = 1;
         for (let n = 0; n <= randomPartyArray.length; n++) {
             // console.log(arrayOfChoices[randomPartyArray[n]].name);
@@ -53,59 +49,37 @@ $.when(...pokeParty)
             <p>${arrayOfChoices[randomPartyArray[n]].name}</p>
             </div>`);
             $(`.card-${card} + form .fieldset`).prepend(`<input type="hidden" value="${arrayOfChoices[randomPartyArray[n]].name}">`);
+            // the above prepends input hidden of the value of the generated pokemon name
             card++;
         }
-// aware of n so can go through each pokemon and check if matches any of the,
-        
-        
-
-                // if (userGuess === `${arrayOfChoices[randomPartyArray[n]].name}`) {
-                //     console.log("correct!")
-                // } else {
-                //     console.log("wrong!")
-                // }
-
-                // const userInput1 = document.getElementById('guess-1').value;
-                // console.log(userInput1);
-
-                // const userInput2 = document.getElementById('guess-2').value;
-                // console.log(userInput2);
-
-                // const userInput3 = document.getElementById('guess-3').value;
-                // console.log(userInput3);
-
-                // const userInput4 = document.getElementById('guess-4').value;
-                // console.log(userInput4);
-
-                // const userInput5 = document.getElementById('guess-5').value;
-                // console.log(userInput5);
-
-                // const userInput6 = document.getElementById('guess-6').value;
-                // console.log(userInput6);
-
-        
-
-        // for (let count = 0; count <= 6; count++) {
-
-        // }
     });
 
         // TO GET 150 and randomize: make a call for 150 and have a giant array of info, then create a function to randomize and grab 6
 
-    // array for each of the index specified above. .when listens to multiple promises and then tells you when the 
-
-
-
-
-
 // stores user input into variable once they click submit
-
-
 // FUNCTION: check if user input matches pokemonName.name
+$(`form`).on(`submit`, function (e) {    
+    e.preventDefault();
+    let userInput = 1;
+
+    // stores in variable value that the user passes in input text from html, and also stores in variable the input hidden (pokemon name) that was dynamically added to the DOM
+    let userGuess = $(this).find('input[type="text"]').val();
+    let correctAnswer = $(this).find('input[type="hidden"]').val();
+    console.log(userGuess);
+    console.log(correctAnswer);
+
+    // checks if userGuess is correct or not
+    if (userGuess === correctAnswer) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+    }
+})
 
 
+// ERROR HANDLING - SOMETIMES NOT ALL POKEMON LOAD ONTO SCREEN
 
-// FUNCTION: when pass, reveal pokemon name
+// FUNCTION: when click pass, reveal pokemon name
 $(`.pass`).on(`click`, function (e) {
     e.preventDefault();
     console.log("pressed");
@@ -115,7 +89,7 @@ $(`.pass`).on(`click`, function (e) {
 });
 
 
-// reset button empties .results and removes choices
+// reset button empties .results and removes choices- NEED TO RELOAD PAGE 
 $(`.reset`).click(function () {
     $(`.results`).empty();
     $(`form`).trigger(`reset`);
@@ -124,43 +98,3 @@ $(`.reset`).click(function () {
 $(function () {
 
 });
-
-// $(`#form-1`).on('submit', function(e) {
-//     e.preventDefault();
-//     console.log("safi");
-// })
-
-$(`form`).on(`submit`, function (e) {
-            
-            e.preventDefault();
-            console.log("harry");
-            // const that = $(this).find('input');
-            
-            // try to run this in a for loop with same logic as abv by let userInput=1
-
-            let userInput = 1;
-                // for (let inputNumber = 0; inputNumber < 6; inputNumber++) {
-                    // need to define 6 userGuess's - how do we store them all in seperate 6 varaibles so we can if/else them later?
-                    let userGuess = $(this).find('input[type="text"]').val();
-                    let correctAnswer = $(this).find('input[type="hidden"]').val();
-                    console.log(userGuess);
-                    console.log(correctAnswer);
-                    if (userGuess === correctAnswer) {
-                        console.log("correct");
-                    } else {
-                        console.log("incorrect");
-                    }
-                    
-
-                    // Checks to see if the userGuess matches the name of the pokemon, then clears userGuess for the next check
-                    // Not working because we use vanilla javascript to get userGuess, but .empty is a jquery function
-                    // Possibly take this out ?
-
-                    // userInput ++;
-
-                    // that.val('');
-                // }
-
-
-        })
-// ERROR HANDLING - SOMETIMES NOT ALL POKEMON LOAD ONTO SCREEN
